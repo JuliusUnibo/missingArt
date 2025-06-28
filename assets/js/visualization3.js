c22db13dd76d876809254d4cbda5f5632f0f8b24
JuliusUnibo <julius.reinmuth@studio.unibo.it>
2025-06-28 21:12:04 +0200

    Update visualization3.js

--- assets/js/visualization3.js
+++ assets/js/visualization3.js
@@ -1,9 +1,8 @@
-// set the dimensions and margins of the graph
+// Set dimensionsmargins of the graph and the svg object
 var margin = {top: 10, right: 30, bottom: 70, left: 140},
     width = 460 - margin.left - margin.right,
     height = 500 - margin.top - margin.bottom;
 
-// append the svg object to the body of the page
 var svg = d3.select("#missingArt3")
   .append("svg")
     .attr("width", width + margin.left + margin.right)
@@ -11,37 +10,36 @@ var svg = d3.select("#missingArt3")
   .append("g")
     .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
 
-// Create a DSV parser for semicolon-separated values
+// Semicolon-separated data
 var dsv = d3.dsvFormat(";");
 
-// Read the data with semicolon as delimiter
+// Read the data
 d3.text("https://raw.githubusercontent.com/JuliusUnibo/missingArt/main/missingArt3.csv", function(text) {
     var data = dsv.parse(text);
 
-  // sort data
+  // Sort data
   data.sort(function(a, b) {
     return b.anzahl_artworks - a.anzahl_artworks;
   });
   
-// limit to top 10 values
+// Limit the data to top 12 values
 data = data.slice(0, 12);
 
 
 
-  // X axis
+  // Initialize X axis
   var x = d3.scaleLinear()
     .domain([0, d3.max(data, function(d) { return d.anzahl_artworks; })])
     .domain([0, 65])
     .range([0, width]);
   svg.append("g")
     .attr("transform", "translate(0," + height + ")")
-    // .call(d3.axisBottom(x).tickFormat(d3.format("d"))) // Remove commas klappt so nicht
     .call(d3.axisBottom(x).tickFormat(d3.format(".0f"))) // Remove commas
     .call(d3.axisBottom(x))
     .selectAll("text")
       .style("fill", "white");
 
-  // Y axis
+  // Initialize Y axis
   var y = d3.scaleBand()
     .range([0, height])
     .domain(data.map(function(d) { return d.genre; }))
@@ -51,7 +49,7 @@ data = data.slice(0, 12);
     .selectAll("text")
       .style("fill", "white");
 
-  // Set the color of the axes to white
+  // Set the color of the axes
   svg.selectAll(".domain, .tick line")
     .attr("stroke", "white")
     .attr("stroke-width", "1"); // Make the axis line thinner
